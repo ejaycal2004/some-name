d3ae147c23cf51b05ef6e97b242f32bc758e8f95
Elijah Calip <ecalip2022@208-Engineering-4.local>
2019-04-03 11:07:12 -0700

    Initial Commit

---
+++ some name/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  some name
+//
+//  Created by Elijah Calip on 4/3/19.
+//  Copyright © 2019 Elijah Calip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
